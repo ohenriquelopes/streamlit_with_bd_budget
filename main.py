cf6c88e966b0c66d3de236aa5d4496821a53a2e2
henrique <hlopessantos52@gmail.com>
2023-10-08 18:09:12 -0300

    connect to test schema

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ pymysql.install_as_MySQLdb()
 conn = st.experimental_connection('mysql', type='sql')
 
 # Perform query.
-df = conn.query('SELECT * from mytable;', ttl=600)
+df = conn.query('SELECT * from test.table;', ttl=600)
 
 # Print results.
 for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+    # st.write(f"{row.name} has a :{row.pet}:")
+    st.write(f"{row.coluna}")
