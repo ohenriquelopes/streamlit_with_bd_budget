2dfe098ec66602adcbfef1627ff88d1ba3ac754b
henrique <hlopessantos52@gmail.com>
2023-10-09 08:51:48 -0300

    insert working

--- controller/cliente.py
+++ controller/cliente.py
@@ -2,6 +2,7 @@
 # import services.database as db
 import streamlit as st
 import pymysql
+from sqlalchemy import text
 # função para selecionar todos os registros no banco de dados
 
 pymysql.install_as_MySQLdb()
@@ -13,22 +14,12 @@ def select_all():
     return df
 
 
-# def insert(input_name, input_age, input_job):
-#     # Inicia uma sessão dentro da conexão
-#     with conn.session as s:
-#         # Use placeholders para inserir os valores de forma segura na consulta SQL
-#         query = 'INSERT INTO test.pessoas VALUES (NULL, :input_name, :input_age, :input_job);'
-#         s.execute(query, params={'nome': input_name, 'idade': input_age, 'job': input_job})
-#         s.commit()
-#         # Retorna uma mensagem de sucesso
-#     return st.write("Inserido com sucesso")
-
-
-# def insert(input_name, input_age, input_job):
-#     with conn.session as s:
-#         # query = 'INSERT INTO test.pessoas VALUES (NULL, :input_name, :input_age, :input_job);'
-#         # s.execute(query, params={'input_name': input_name, 'input_age': input_age, 'input_job': input_job})
-#         s.execute("INSERT INTO test.pessoas (id, nome, idade, job) VALUES (NULL, 'a', 1, 'a');")
-#         s.commit()
-#     return st.write("Inserido com sucesso")
+def insert(input_name1, input_age, input_job):
+    # Inicia uma sessão dentro da conexão
+    with conn.session as s:
+        # Use placeholders para inserir os valores de forma segura na consulta SQL
+        s.execute(text('INSERT INTO test.pessoas (nome, idade, job) VALUES (:input_name2, :input_age, :input_job);'), params={'input_name2': input_name1, 'input_age': input_age, 'input_job': input_job})
+        s.commit()
+        # Retorna uma mensagem de sucesso
+    return st.write("Inserido com sucesso")
 
